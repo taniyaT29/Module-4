e2cceee30ff203c148698349af9a0b906247af67
taniyaT29 <tanu29taniya@gmail.com>
2019-12-01 22:20:30 +0530

    Final Solutions

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-console.log("Hello Coursera");
 
+
+(function () {
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+
+for (var i = 0; i < names.length; i++)
+{
+
+  var firstAlpha = names[i].charAt(0);
+  var Alpha = firstAlpha.toLowerCase();
+
+  if (Alpha == "j")
+  {
+    byeSpeaker.speak(names[i]);
+  }
+  else{
+
+    helloSpeaker.speak(names[i]);
+
+  }
+} 
+})();
+
+ 
